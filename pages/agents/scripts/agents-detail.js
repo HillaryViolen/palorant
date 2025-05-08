ca50603bb760bfb4cfe4329174e03c4d89ddb605
HillaryViolen <vviolenhillary@gmail.com>
2025-05-08 08:31:11 +0700

    update 3

--- pages/agents/scripts/agents-detail.js
+++ pages/agents/scripts/agents-detail.js
@@ -1,4 +1,157 @@
-import { agentsData } from "./agentsData.js";
+const agentsData = [
+    {
+        name: "jeht",
+        abilities: [
+            {
+                id: 'ability_1',
+                name: "CLOUDBURST",
+                description: "INSTANTLY throw a projectile that expands into a brief vision-blocking cloud on impact with a surface. HOLD the ability key to curve the smoke in the direction of your crosshair."
+            },
+            {
+                id: 'ability_2',
+                name: "UPDRAFT",
+                description: "INSTANTLY propel Jett high into the air."
+            },
+            {
+                id: 'ability_3',
+                name: "TAILWIND",
+                description: "ACTIVATE to prepare a gust of wind for a limited time. RE-USE the wind to propel Jett in the direction she is moving. If Jett is standing still, she propels forward. Tailwind charge resets every two kills."
+            },
+            {
+                id: 'ability_4',
+                name: "BLADE STORM",
+                description: "EQUIP a set of highly accurate throwing knives. FIRE to throw a single knife and recharge knives on a kill. ALT FIRE to throw all remaining daggers but does not recharge on a kill."
+            },
+        ]
+    },
+    {
+        name: "deathlog",
+        abilities: [
+            {
+                id: 'ability_1',
+                name: "ANNIHILATION",
+                description: "EQUIP a Nanowire Accelerator. FIRE to unleash a pulse of nanowires that captures the first enemy contacted. The cocooned enemy is pulled along a nanowire path and will die if they reach the end, unless they are freed. The nanowire cocoon is destructible."
+            },
+            {
+                id: 'ability_2',
+                name: "BARRIER MESH",
+                description: "EQUIP a Barrier Mesh disc. FIRE to throw forward. Upon landing, the disc generates barriers from the origin point that block character movement."
+            },
+            {
+                id: 'ability_3',
+                name: "GRAVNET",
+                description: "EQUIP a GravNet grenade. FIRE to throw. ALT FIRE to lob the grenade underhand. The GravNet detonates upon landing, forcing any enemies caught within to crouch and move slowly."
+            },
+            {
+                id: 'ability_4',
+                name: "SONIC SENSOR",
+                description: "EQUIP a Sonic Sensor. FIRE to deploy. The sensor monitors an area for enemies making sound. It concusses that area if footsteps, weapons fire, or significant noise are detected."
+            },
+        ]
+    },
+    {
+        name: "phynex",
+        abilities: [
+            {
+                id: 'ability_1',
+                name: "BLAZE",
+                description: "EQUIP a flame wall. FIRE to create a line of flame that moves forward, creating a wall of fire that blocks vision and damages players passing through it. HOLD FIRE to bend the wall in the direction of your crosshair."
+            },
+            {
+                id: 'ability_2',
+                name: "CURVEBALL",
+                description: "EQUIP a flare orb that takes a curving path and detonates shortly after throwing. FIRE to curve the flare orb to the left, detonating and blinding any player who sees the orb. ALTERNATE FIRE to curve the flare orb to the right."
+            },
+            {
+                id: 'ability_3',
+                name: "HOT HANDS",
+                description: "EQUIP a fireball. FIRE to throw a fireball that explodes after a set amount of time or upon hitting the ground, creating a lingering fire zone that damages enemies."
+            },
+            {
+                id: 'ability_4',
+                name: "RUN IT BACK",
+                description: "INSTANTLY place a marker at Phoenix’s location. While this ability is active, dying or allowing the timer to expire will end this ability and bring Phoenix back to this location with full health."
+            },
+        ]
+    },
+    {
+        name: "vyz",
+        abilities: [
+            {
+                id: 'ability_1',
+                name: "ARC ROSE",
+                description: "EQUIP an Arc Rose. Target a surface and FIRE to place a stealthed Arc Rose, or ALT Fire to place the Arc Rose through it. REUSE to blind all players looking at it. This ability can be picked up to be REDEPLOYED."
+            },
+            {
+                id: 'ability_2',
+                name: "RAZORVINE",
+                description: "EQUIP a nest of liquid metal. FIRE to launch. Upon landing, the nest becomes invisible. When ACTIVATED, it sprawls out into a large razorvine nest which slows and damages all players who move through it."
+            },
+            {
+                id: 'ability_3',
+                name: "SHEAR",
+                description: "EQUIP filaments of liquid metal. FIRE to place a hidden wall trap. When an enemy crosses, an indestructible wall bursts from the ground behind them. The wall lasts for a brief time before dissipating."
+            },
+            {
+                id: 'ability_4',
+                name: "STEEL GARDEN",
+                description: "EQUIP a bramble of liquid metal. FIRE to send the metal erupting from you as a torrent of metal thorns, JAMMING enemy primary weapons after a brief windup."
+            },
+        ]
+    },
+    {
+        name: "raez",
+        abilities: [
+            {
+                id: 'ability_1',
+                name: "BLAST PACK",
+                description: "INSTANTLY throw a Blast Pack that will stick to surfaces. RE-USE the ability after deployment to detonate, damaging and moving anything hit. Raze isn't damaged by this ability, but will still take fall damage if launched up far enough."
+            },
+            {
+                id: 'ability_2',
+                name: "BOOM BOT",
+                description: "EQUIP a Boom Bot. FIRE will deploy the bot, causing it to travel in a straight line on the ground, bouncing off walls. The Boom Bot will lock on to any enemies in its frontal cone and chase them, exploding for heavy damage if it reaches them."
+            },
+            {
+                id: 'ability_3',
+                name: "PAINT SHELLS",
+                description: "EQUIP a cluster grenade. FIRE to throw the grenade, which does damage and creates sub-munitions, each doing damage to anyone in their range. ALT FIRE to lob. Paint Shells charge resets every two kills."
+            },
+            {
+                id: 'ability_4',
+                name: "SHOWSTOPPER",
+                description: "EQUIP a rocket launcher. FIRE to shoot a rocket that does massive area damage on contact with anything."
+            },
+        ]
+    },
+    {
+        name: "chameon",
+        abilities: [
+            {
+                id: 'ability_1',
+                name: "HEADHUNTER",
+                description: "ACTIVATE to equip a heavy pistol. ALT FIRE with the pistol equipped to aim down sights."
+            },
+            {
+                id: 'ability_2',
+                name: "RENDEZVOUS",
+                description: "EQUIP a teleport anchor. FIRE to place it on the ground. While on the ground and in range of the anchor, REACTIVATE to quickly teleport to the anchor. The anchor can be picked up to be REDEPLOYED.EQUIP a teleport anchor. FIRE to place it on the ground. While on the ground and in range of the anchor, REACTIVATE to quickly teleport to the anchor. The anchor can be picked up to be REDEPLOYED."
+            },
+            {
+                id: 'ability_3',
+                name: "TOUR DE FORCE",
+                description: "ACTIVATE to summon a powerful, custom sniper rifle that will kill an enemy with any direct hit to the upper body. ALT FIRE to aim down sights. Killing an enemy creates a lingering field that Slows players caught inside of it."
+            },
+            {
+                id: 'ability_4',
+                name: "TRADEMARK",
+                description: "EQUIP a trap that scans for enemies. FIRE to place it on the ground. When a visible enemy comes in range, the trap counts down and then destabilizes the terrain around them, creating a lingering field that Slows players caught inside of it. The trap can be picked up to be REDEPLOYED."
+            },
+        ]
+    }
+];
+
+
 
 const agentName = document.querySelector('.agent-name');
 const videoContainer = document.querySelector(".video__frame video");
